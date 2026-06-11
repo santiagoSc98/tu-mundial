0586590e0c0259ac357dafc1d9b093261dd9004c
Santiago Campuzano <82460097+santiagoSc98@users.noreply.github.com>
2026-06-11 08:28:54 -0300

    fix: node version for vercel build

--- app/join/[code]/page.tsx
+++ app/join/[code]/page.tsx
@@ -1,16 +1,9 @@
-import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
-export default async function JoinPage({
+export default function JoinPage({
   params,
 }: {
   params: { code: string }
 }) {
-  const cookieStore = await cookies()
-  cookieStore.set('pending_join_code', params.code.toUpperCase(), {
-    maxAge: 60 * 30,
-    path: '/',
-  })
-
-  redirect('/home')
+  redirect(`/home?join=${params.code}`)
 }
